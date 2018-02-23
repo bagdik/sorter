e062172c2f5bcae4d5e7f797e803282b99a104f2
bagdik <bahdanchik@tut.by>
2018-02-23 23:29:01 +0300

    v1

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
 class Sorter {
   constructor() {
     // your implementation
+	this.arr = new Array();
+	this.comparator = function(a, b){
+		return a - b;
+	}
   }
 
   add(element) {
     // your implementation
+	this.arr.push(element);
   }
 
   at(index) {
     // your implementation
+	return this.arr[index];
   }
 
   get length() {
     // your implementation
+	return this.arr.length;
   }
 
   toArray() {
     // your implementation
+	return this.arr;
   }
 
+ 
   sort(indices) {
-    // your implementation
+    indices.sort((a,b) => a - b)
+    let tmp = [];
+    indices.forEach(i => {
+       tmp.push(this.at(i)); 
+    });
+    tmp.sort(this.comparator);
+    for (let i = 0; i < tmp.length; i++) {
+      this.arr[indices[i]] = tmp[i];
+    }
   }
 
   setComparator(compareFunction) {
     // your implementation
+	this.comparator = compareFunction;
   }
 }
 
